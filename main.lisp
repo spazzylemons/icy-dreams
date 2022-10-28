c877836c227d059efc47bf6d9a628a799f723908
spazzylemons <25674682+spazzylemons@users.noreply.github.com>
2022-10-28 11:33:53 -0400

    begin to implement game objects

--- main.lisp
+++ main.lisp
@@ -3,13 +3,45 @@
 (defparameter *screen-width* 256)
 (defparameter *screen-height* 224)
 
+;; game objects
+(defstruct game-object x y id)
+
+(defparameter *game-objects* nil)
+
+; temporary
+(defvar *test-object*)
+
+(defun spawn ()
+  (let ((result (make-game-object :x 0.0 :y 0.0 :id (gensym))))
+    (push result *game-objects*)
+    result))
+
+(defun despawn (obj)
+  (delete obj *game-objects*))
+
+(defun update-game ()
+  (if (raylib:is-key-down raylib:+key-left+)
+    (decf (game-object-x *test-object*)))
+  (if (raylib:is-key-down raylib:+key-right+)
+    (incf (game-object-x *test-object*)))
+  (if (raylib:is-key-down raylib:+key-up+)
+    (decf (game-object-y *test-object*)))
+  (if (raylib:is-key-down raylib:+key-down+)
+    (incf (game-object-y *test-object*))))
+
 (defun render-game ()
   (raylib:clear-background raylib:+white+)
-  (raylib:draw-text "hello world!" 10 10 20 raylib:+red+))
+  (raylib:draw-text "hello world!" 10 10 20 raylib:+red+)
+  (dolist (obj *game-objects*)
+    (raylib:draw-text "Object" (round (game-object-x obj))
+                               (round (game-object-y obj))
+                               20
+                               raylib:+green+)))
 
 (defun main-loop (target-texture)
   (loop
     (if (raylib:window-should-close) (return))
+    (update-game)
     (let* ((scale (min (/ (float (raylib:get-screen-width)) *screen-width*)
                       (/ (float (raylib:get-screen-height)) *screen-height*)))
            (width-scale (* scale *screen-width*))
@@ -28,6 +60,14 @@
       (raylib:with-drawing
         (raylib:clear-background raylib:+black+)
         (raylib:draw-texture-pro texture src-rect dst-rect (3d-vectors:vec 0.0 0.0) 0.0 raylib:+white+)))))
+
+(let ((obj-a (spawn))
+      (obj-b (spawn)))
+  (setf (game-object-x obj-a) 10.0)
+  (setf (game-object-y obj-a) 10.0)
+  (setf *test-object* obj-a)
+  (setf (game-object-x obj-b) 40.0)
+  (setf (game-object-y obj-b) 40.0))
 
 (raylib:set-config-flags '(:flag-window-resizable :flag-vsync-hint))
 (raylib:with-window ((* *screen-width* 2) (* *screen-height* 2) "Icy Dreams")
