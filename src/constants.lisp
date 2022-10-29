ff4d511a6e30040a35ba72015c90ac220d741574
spazzylemons <25674682+spazzylemons@users.noreply.github.com>
2022-10-28 20:24:50 -0400

    improve physics logic

--- src/constants.lisp
+++ src/constants.lisp
@@ -5,3 +5,9 @@
 
 ;; The height of a stage, in tiles.
 (defparameter *stage-height* 24)
+
+;; The maximum velocity.
+(defparameter *terminal-velocity* 3.5)
+
+;; The speed of gravity.
+(defparameter *gravity* 0.125)
